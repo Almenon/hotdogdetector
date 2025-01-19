7077eaf5cc89eec2b1e4c0a589e4fc68b2b3aa50
Almenon <almenon214@gmail.com>
2025-01-18 21:30:49 -0800

    switch to microsoft phi

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
 from datetime import datetime
 
-import torch
+from auto_round import AutoRoundConfig ##must import for auto-round format
+import requests
 from PIL import Image
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoProcessor
 
 startdownload = datetime.now()
-# load omni model default, the default init_vision/init_audio/init_tts is True
-# if load vision-only model, please set init_audio=False and init_tts=False
-# if load audio-only model, please set init_vision=False
-model = AutoModel.from_pretrained(
-    'openbmb/MiniCPM-o-2_6',
-    trust_remote_code=True,
-    attn_implementation='sdpa', # sdpa or flash_attention_2
-    torch_dtype=torch.bfloat16,
-    init_vision=True,
-    init_audio=False,
-    init_tts=False
+
+model_id = "OPEA/Phi-3.5-vision-instruct-qvision-int4-sym-inc" 
+
+model = AutoModelForCausalLM.from_pretrained(
+  model_id, 
+  device_map="auto", 
+  trust_remote_code=True, 
+  torch_dtype="auto",
+  # _attn_implementation='flash_attention_2' # FlashAttention only supports Ampere GPUs or newer :(
 )
+processor = AutoProcessor.from_pretrained(model_id, 
+  trust_remote_code=True, 
+  num_crops=16
+)
+
 print(f'download done in {datetime.now()-startdownload} seconds')
 
-modelinitialized = datetime.now()
-model = model.eval().cuda()
-tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-o-2_6', trust_remote_code=True)
-print(f'model initialized in {datetime.now()-modelinitialized} seconds')
+image_url = "https://images.pexels.com/photos/4676409/pexels-photo-4676409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+content = "Is this a hotdog? Just answer yes or no. "
+messages = [
+    {"role": "user", 
+     "content": "<|image_1|>\n"+content},
+]
 
-msgs = [{
-    "role": "user",
-    "content": ["What is one plus one?"]
-}]
+prompt = processor.tokenizer.apply_chat_template(
+  messages, 
+  tokenize=False, 
+  add_generation_prompt=True
+)
 
+image_inputs = Image.open(requests.get(image_url, stream=True).raw)
 aistart = datetime.now()
-answer = model.chat(msgs=msgs, tokenizer=tokenizer)
+inputs = processor(prompt, image_inputs, return_tensors="pt").to(model.device) 
+
+generation_args = { 
+    "max_new_tokens": 1000, 
+    "temperature": 0.0, 
+    "do_sample": False, 
+} 
+
+generate_ids = model.generate(**inputs, 
+  eos_token_id=processor.tokenizer.eos_token_id, 
+  **generation_args
+)
+
+# remove input tokens 
+generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
+response = processor.batch_decode(generate_ids, 
+  skip_special_tokens=True, 
+  clean_up_tokenization_spaces=False)[0] 
+
+print(response)
 print(f'ai thinking complete in {datetime.now()-aistart} seconds')
-print(answer)
--- readme.md
+++ readme.md
@@ -1,14 +1,18 @@
 # Requirements
-Linux or Windows should both work, but if you use Windows make sure to use WSL.
-If you're dead set on using native Windows then you either need to compile flash attention 2 yourself (which takes a very long time unless you have a powerful CPU)
-or you need to use a prebuilt wheel from https://github.com/oobabooga/flash-attention/releases. I recommend using WSL instead because there's a lot of tools out there that are a pain to install on windows or simply not able to run on Windows. Speaking from experience.
+Linux or Windows should both work... theoretically. I know it works on WSL.
 
-You need to use python 3.10
+I used python 3.10, other versions may or may not work.
 
 # Setup
 Run `python -m pip install -r install_first.txt` to download Pytorch.
 Then run Run `python -m pip install -r install_second.txt` to download the other libraries.
-The requirements use pre-compiled wheels compiled with CUDA 12. Be prepare for a large download (~3GB).
+The requirements use pre-compiled wheels compiled with CUDA 12. Be prepared for a large download.
 
 # Usage
 On first run be prepared for another large download (~16GB)
+
+## Flash attention (optional)
+
+If you want to use flash attention, I recommend using WSL or Linux. If you *reallly* want to use Windows you can use the prebuilt wheel from https://github.com/oobabooga/flash-attention/releases. 
+
+Generally speaking for Python it's good to use WSL or Linux because there's a lot of tools out there that are a pain to install on windows or simply not able to run on Windows. Speaking from experience.
